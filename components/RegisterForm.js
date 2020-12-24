31bd41917966997350ceb41dccd08ad74aa43721
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2020-12-24 09:40:06 -0600

    Add loading and error handling to registerform

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -1,21 +1,43 @@
 import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import registerEmail from '../utils/registerEmail'
 
 const RegisterForm = () => {
     const [email, setEmail] = useState('')
+    const [name, setName] = useState('')
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    const router = useRouter()
 
     const handleInput = (event) => {
         setEmail(event.target.value)
     }
 
-    const handleSubmit = (event) => {
-
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        const formData = new FormData(event.target)
+        let formValues = {}
+        formData.forEach((value, key) => formValues[key] = value)
+        setLoading(true)
+        const response = await registerEmail(formValues)
+        if(response.status===201){
+            setLoading(false)
+            setError('')
+            router.push('/confirma-tu-correo/')
+        } else {
+            setLoading(false)
+            setError("Hubo un error al registrarte, por favor intenta de nuevo")
+        }
     }
 
     return (
         <form onSubmit={handleSubmit}>
-            <input id="email" onChange={handleInput} value={email} className="border border-gray-300 text-lg sm:w-full text-gray-500 mt-8 py-4 px-4 rounded-xl" type="email" placeholder="Dinos a que email te avisamos" required/>
+            <label htmlFor="email"></label>
+            <input disabled={loading} id="email" onChange={handleInput} name="email" value={email} className="border border-gray-300 text-lg sm:w-full text-gray-500 py-4 mt-4 px-4 rounded-xl" type="email" placeholder="tu@email.com" required/>
             <br/>
-            <button type="submit" className="transition duration-500 hover:shadow-md p-4 mt-4 rounded-xl text-lg bg-red-500 hover:bg-red-400 text-white transform hover:-translate-y-1 hover:scale-105">Regístrame ya</button>
+            <div className="text-red-600 text-lg">{error}</div>
+            <button disabled={loading} type="submit" className="hover:shadow-md p-4 mt-4 rounded-xl text-lg bg-gradient-to-r from-red-500 to-red-500 hover:from-red-500 hover:to-yellow-600 text-white">Regístrame ya</button>
         </form>  
         )
 }
