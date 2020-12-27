a57a808861f48b27ba2062377b6b91c49cce3e18
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2020-12-26 22:23:07 -0600

    Include Facebook Pixel to _app with its preconnect headers

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import '../styles.css';
 import Layout from '../components/Layout'
+import FacebookPixel from '../components/FacebookPixel'
+import Head from 'next/head'
 
 function App({ Component, pageProps }) {
+
   return (
     <Layout>
+        <Head>
+            <link rel="preconnect" href="https://connect.facebook.net" crossorigin />
+            <link rel="dns-prefetch" href="https://connect.facebook.net" />
+        </Head>
+        <FacebookPixel/>
         <Component {...pageProps} />
     </Layout>
     )
