64486001270e343608d8b71fcd4a61650b92d504
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2020-12-26 22:15:03 -0600

    Include a FacebookPixel Component and events
    The FacebookPixel Component executes the facebook events function
    obtained directly from facebook, the facebook pixel is obtained from the
    settings file. Also a fb track event function is added, a new fb Page
    view event is triggered on every route change.

--- settings/settings.js
+++ settings/settings.js
@@ -1,7 +1,8 @@
 const settings = {
-  LIST_ID : process.env.LIST_ID
+  LIST_ID : process.env.LIST_ID,
   MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY,
   MAILCHIMP_PREFIX_SERVER: process.env.MAILCHIMP_PREFIX_SERVER,
+  FB_PIXEL: process.env.FB_PIXEL,
 }
 
 export default settings
---
+++ utils/fbTrackEvent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const fbTrackEvent = (event, data=null) => {
+  if(typeof fbq === "function"){
+    fbq('track', event, data)
+    return true
+  }
+  return false
+}
+
+export default fbTrackEvent
