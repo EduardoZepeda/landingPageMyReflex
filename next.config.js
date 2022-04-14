b2617a090c439a37f8eb9ddc1490ee2236ebb896
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-04-14 00:23:12 -0500

    Fix missing CSS files in github pages
    Github pages expects the static to be prefixed with the path that
    corresponds to the project url, since we're using a post 9.4 nextjs
    version the solution is as simple as just modifying the nextjs config
    file and add an assetPrefix and a basePath for links components.
    more details here
    https://wallis.dev/blog/deploying-a-next-js-app-to-github-pages

---
+++ next.config.js
@@ -0,0 +1,6 @@
+// Github pages expects the static to be prefixed with this
+// more details here https://wallis.dev/blog/deploying-a-next-js-app-to-github-pages
+module.exports = {
+    assetPrefix: '/landingPageMyReflex/',
+    basePath: '/landingPageMyReflex',
+}
